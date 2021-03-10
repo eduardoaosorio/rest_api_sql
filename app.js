6724be4d3d37a998fb947f43d0a5003365784d79
Eduardo Osorio <eduardoaosorio95@gmail.com>
2021-03-10 13:05:27 -0500

    Verify connection to db

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
-'use strict';
+"use strict";
 
 // load modules
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const db = require("./models/index");
 
 // variable to enable global error logging
-const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
+const enableGlobalErrorLogging =
+  process.env.ENABLE_GLOBAL_ERROR_LOGGING === "true";
 
 // create the Express app
 const app = express();
 
+// setup connection to database and sync
+db.sequelize
+  .authenticate()
+  .then(() => console.log("Connection to the database successful!"))
+  .catch((err) => console.log("Couldn't connect to the database:\n", err));
+// .then(() => db.sequelize.sync())
+// .then(() => console.log("Sync successfull!"))
+// .catch((err) => console.log("Couldn't sync the database:\n", err));
+
 // setup morgan which gives us http request logging
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // setup a friendly greeting for the root route
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    message: 'Welcome to the REST API project!',
+    message: "Welcome to the REST API project!",
   });
 });
 
 // send 404 if no other route matched
 app.use((req, res) => {
   res.status(404).json({
-    message: 'Route Not Found',
+    message: "Route Not Found",
   });
 });
 
@@ -40,9 +51,9 @@ app.use((err, req, res, next) => {
 });
 
 // set our port
-app.set('port', process.env.PORT || 5000);
+app.set("port", process.env.PORT || 5000);
 
 // start listening on our port
-const server = app.listen(app.get('port'), () => {
+const server = app.listen(app.get("port"), () => {
   console.log(`Express server is listening on port ${server.address().port}`);
 });
