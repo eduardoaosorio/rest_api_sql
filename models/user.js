0fd2492ed545bda6b19d3728c4fbb8b83dd936ea
Eduardo Osorio <eduardoaosorio95@gmail.com>
2021-03-11 13:24:48 -0500

    Add sequelize hook to hash password after validation success

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@
 
 const Sequelize = require("sequelize");
 const { Model } = Sequelize;
+const bcrypt = require("bcrypt");
 
 module.exports = (sequelize) => {
   class User extends Model {}
@@ -56,12 +57,12 @@ module.exports = (sequelize) => {
           notEmpty: {
             msg: "Please provide a password",
           },
-          // is: {
-          //   // regex source: https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
-          //   args: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/],
-          //   msg:
-          //     "Password must have a minimum of 8 characters, at least 1 uppercase letter, 1 lowercase letter and 1 number",
-          // },
+          is: {
+            // regex source: https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+            args: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/],
+            msg:
+              "Password must have a minimum of 8 characters, at least 1 uppercase letter, 1 lowercase letter and 1 number",
+          },
         },
       },
     },
@@ -70,6 +71,17 @@ module.exports = (sequelize) => {
       modelName: "User",
     }
   );
+
+  // sequelize hook, runs after successful validation to hash password
+  User.addHook("afterValidate", async (user) => {
+    try {
+      console.log("Hook just run");
+      user.password = await bcrypt.hash(user.password, 10);
+      return "Password hashed successfully!";
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
   User.associate = (models) => {
     User.hasMany(models.Course, {
