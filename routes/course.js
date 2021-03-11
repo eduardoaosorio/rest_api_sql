2fb3b6f169e3b69a1fd74194fb3091b09bf33d61
Eduardo Osorio <eduardoaosorio95@gmail.com>
2021-03-11 17:44:54 -0500

    Clean up routes, and include new middleware

--- routes/course.js
+++ routes/course.js
@@ -3,7 +3,12 @@
 const express = require("express");
 const router = express.Router();
 const { Course, User } = require("../models");
-const { authenticateUser, catchAsync } = require("../middleware");
+const {
+  checkCourseExistance,
+  authenticateUser,
+  verifyOwner,
+  catchAsync,
+} = require("../middleware");
 
 // route will return a list of all courses including the User that owns each course and a 200 HTTP status code
 router.get(
@@ -29,11 +34,10 @@ router.get(
   })
 );
 
-// VER EL ERROR HANDLING SI DEPRONTO TOCA PASARLO A OTRO LADO
-
 // route will return the corresponding course along with the User that owns that course and a 200 HTTP status code
 router.get(
   "/courses/:id",
+  checkCourseExistance,
   catchAsync(async (req, res, next) => {
     const course = await Course.findByPk(req.params.id, {
       attributes: [
@@ -51,17 +55,9 @@ router.get(
         },
       ],
     });
-    if (course) {
-      res.status(200).json(course);
-    } else {
-      res
-        .status(404)
-        .json({ message: "Supplied ID doesn't match any existing course" });
-    }
+    res.status(200).json(course);
   })
 );
-
-// VER EL ERROR HANDLING SI DEPRONTO TOCA PASARLO A OTRO LADO
 
 // route will create a new course, set the Location header to the URI for the newly created course, and return a 201 HTTP status code and no content
 router.post(
@@ -77,12 +73,13 @@ router.post(
 // route will update the corresponding course and return a 204 HTTP status code and no content
 router.put(
   "/courses/:id",
+  checkCourseExistance,
   authenticateUser,
+  verifyOwner,
   catchAsync(async (req, res, next) => {
     const { id: userId } = req.user;
-    const course = await Course.findByPk(req.params.id);
+    const { course } = req;
     const updatedCourse = await course.update({ ...req.body, userId });
-    console.log(updatedCourse.toJSON());
     res.location(`/courses/${updatedCourse.id}`).status(204).end();
   })
 );
@@ -90,9 +87,11 @@ router.put(
 // route will delete the corresponding course and return a 204 HTTP status code and no content
 router.delete(
   "/courses/:id",
+  checkCourseExistance,
   authenticateUser,
+  verifyOwner,
   catchAsync(async (req, res, next) => {
-    const course = await Course.findByPk(req.params.id);
+    const { course } = req;
     await course.destroy();
     res.status(204).end();
   })
